a51b17e794fa3e78f828b736909fcc66b05607d8
fde <fde>
2023-11-24 11:49:10 +0800

    comment adb_connect invalid ip

--- tools/services/user_manager.py
+++ tools/services/user_manager.py
@@ -66,10 +66,9 @@ def start(args, session, unlocked_cb=None):
 
     def userUnlocked(uid):
         logging.info("Android with user {} is ready".format(uid))
-
+        """
         tools.helpers.net.adb_connect(args)
 
-        """
         platformService = IPlatform.get_service(args)
         if platformService:
             if not os.path.exists(apps_dir):
