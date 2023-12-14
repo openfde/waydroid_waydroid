8028ec0166b1b406d6466984ecb2d1d91c2d649b
tb <ali609386314@gmail.com>
2023-12-14 11:51:44 +0800

    add service to implement network-related interfaces

---
+++ tools/interfaces/INet.py
@@ -0,0 +1,114 @@
+import gbinder
+import logging
+from tools import helpers
+from gi.repository import GLib
+
+
+INTERFACE = "lineageos.waydroid.INet"
+SERVICE_NAME = "fdenet"
+
+TRANSACTION_setStaticIp = 1
+TRANSACTION_setDHCP = 2
+TRANSACTION_getAllSsid = 3
+TRANSACTION_connectSsid = 4
+TRANSACTION_getActivedWifi = 5
+TRANSACTION_connectActivedWifi = 6
+TRANSACTION_enableWifi = 7
+TRANSACTION_connectedWifiList = 8
+TRANSACTION_isWifiEnable = 9
+TRANSACTION_getSignalAndSecurity = 10
+TRANSACTION_connectHidedWifi = 11
+
+def add_service(args, setStaticIp, setDHCP, getAllSsid, connectSsid, getActivedWifi, 
+    connectActivedWifi, enableWifi, connectedWifiList, isWifiEnable, getSignalAndSecurity, connectHidedWifi):
+    helpers.drivers.loadBinderNodes(args)
+    try:
+        serviceManager = gbinder.ServiceManager("/dev/" + args.BINDER_DRIVER, args.SERVICE_MANAGER_PROTOCOL, args.BINDER_PROTOCOL)
+    except TypeError:
+        serviceManager = gbinder.ServiceManager("/dev/" + args.BINDER_DRIVER)
+
+    def response_handler(req, code, flags):
+        logging.debug(
+            "{}: Received transaction: {}".format(SERVICE_NAME, code))
+        reader = req.init_reader()
+        local_response = response.new_reply()
+        if code == TRANSACTION_setStaticIp:
+            arg1 = reader.read_string16()
+            arg2 = reader.read_string16()
+            status, arg3 = reader.read_int32()
+            arg4 = reader.read_string16()
+            arg5 = reader.read_string16()
+            arg6 = reader.read_string16()
+            ret = setStaticIp(arg1, arg2, arg3, arg4, arg5, arg6)
+            local_response.append_int32(0)
+            local_response.append_int32(ret)
+        if code == TRANSACTION_setDHCP:
+            arg1 = reader.read_string16()
+            ret = setDHCP(arg1)
+            local_response.append_int32(0)
+            local_response.append_int32(ret)
+        if code == TRANSACTION_getAllSsid:
+            ret = getAllSsid()
+            local_response.append_int32(0)
+            local_response.append_string16(ret)
+        if code == TRANSACTION_connectSsid:
+            arg1 = reader.read_string16()
+            arg2 = reader.read_string16()
+            ret = connectSsid(arg1, arg2)
+            local_response.append_int32(0)
+            local_response.append_int32(ret)
+        if code == TRANSACTION_getActivedWifi:
+            ret = getActivedWifi()
+            local_response.append_int32(0)
+            local_response.append_string16(ret)
+        if code == TRANSACTION_connectActivedWifi:
+            arg1 = reader.read_string16()
+            status, arg2 = reader.read_int32()
+            ret = connectActivedWifi(arg1, arg2)
+            local_response.append_int32(0)
+            local_response.append_int32(ret)
+        if code == TRANSACTION_enableWifi:
+            status, arg1 = reader.read_int32()
+            ret = enableWifi(arg1)
+            local_response.append_int32(0)
+            local_response.append_int32(ret)
+        if code == TRANSACTION_connectedWifiList:
+            ret = connectedWifiList()
+            local_response.append_int32(0)
+            local_response.append_string16(ret)
+        if code == TRANSACTION_isWifiEnable:
+            ret = isWifiEnable()
+            local_response.append_int32(0)
+            local_response.append_int32(ret)
+        if code == TRANSACTION_getSignalAndSecurity:
+            arg1 = reader.read_string16()
+            ret = getSignalAndSecurity(arg1)
+            local_response.append_int32(0)
+            local_response.append_string16(ret)
+        if code == TRANSACTION_connectHidedWifi:
+            arg1 = reader.read_string16()
+            arg2 = reader.read_string16()
+            ret = connectHidedWifi(arg1, arg2)
+            local_response.append_int32(0)
+            local_response.append_int32(ret)
+        return local_response, 0
+
+    def binder_presence():
+        if serviceManager.is_present():
+            status = serviceManager.add_service_sync(SERVICE_NAME, response)
+
+            if status:
+                logging.error("Failed to add service {}: {}".format(
+                    SERVICE_NAME, status))
+                args.netLoop.quit()
+
+    response = serviceManager.new_local_object(INTERFACE, response_handler)
+    args.netLoop = GLib.MainLoop()
+    binder_presence()
+    status = serviceManager.add_presence_handler(binder_presence)
+    if status:
+        args.netLoop.run()
+        serviceManager.remove_handler(status)
+        del serviceManager
+    else:
+        logging.error("Failed to add presence handler: {}".format(status))
---
+++ tools/services/net_manager.py
@@ -0,0 +1,189 @@
+# Copyright 2021 Erfan Abdi
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""
+ The service to implement network-related interfaces
+"""
+import logging
+import threading
+import tools.actions.container_manager
+import tools.actions.session_manager
+import tools.config
+import tools.helpers.run
+import time
+
+import subprocess
+
+
+from tools import helpers
+from tools.interfaces import INet
+
+stopping = False
+
+def start(args):
+    def run_nmcli_command(command):
+        try:
+            output = subprocess.check_output(command, shell=True, universal_newlines=True)
+            if output:
+                return output.strip()
+            else:
+                return "success"
+        except subprocess.CalledProcessError as e:
+            #logging.debug("Error executing nmcli command:", e)
+            return None
+    def setStaticIp(interfaceName, ipAddress, networkPrefixLength, gateway, dns1, dns2):
+        logging.debug(
+            "interfaceName: {}, ipAddress: {}, networkPrefixLength: {}, gateway: {}, dns1: {}, dns2: {}" \
+            .format(interfaceName, ipAddress, networkPrefixLength, gateway, dns1, dns2))
+        output = run_nmcli_command("nmcli con modify OpenfdeEthernet ifname " + interfaceName + " ipv4.addresses " + ipAddress + "/" + str(networkPrefixLength) + " ipv4.gateway " + gateway + " ipv4.dns '" + dns1 + " " + dns2 + "' ipv4.method 'manual'")
+        logging.debug(output)
+        if not output:
+            logging.debug("nmcli con add:")
+            output = run_nmcli_command("nmcli con add con-name OpenfdeEthernet ifname " + interfaceName + "  type ethernet ip4 " + ipAddress + "/" + str(networkPrefixLength) + " gw4 " + gateway + " ipv4.dns '" + dns1 + " " + dns2 + "' ipv4.method 'manual'")
+        output = run_nmcli_command("nmcli connection up OpenfdeEthernet")
+        if output:
+            logging.debug("set setStaticIp success")
+            return 0
+        else:
+            logging.debug("set setStaticIp fail")
+            return 1
+    def setDHCP(interfaceName):
+        logging.debug("interfaceName: {}".format(interfaceName))
+        output = run_nmcli_command("nmcli con modify OpenfdeEthernet ifname " + interfaceName + " ipv4.method 'auto'")
+        logging.debug(output)
+        if not output:
+            logging.debug("nmcli con add:")
+            output = run_nmcli_command("nmcli con add con-name OpenfdeEthernet ifname " + interfaceName + " ipv4.method 'auto'")
+        output = run_nmcli_command("nmcli connection up OpenfdeEthernet")
+        if output:
+            logging.debug("set setDHCP success")
+            return 0
+        else:
+            logging.debug("set setDHCP fail")
+            return 1
+    def getAllSsid():
+        logging.debug("getAllSsid")
+        output = run_nmcli_command("nmcli -t -f ssid dev wifi|sed '/^$/d'")
+        logging.debug(output)
+        if output == "success":
+            logging.debug("none any Ssid")
+            return None
+        elif output:
+            logging.debug("getAllSsid success")
+            return output
+        else:
+            logging.debug("getAllSsid fail")
+            return None
+    def connectSsid(ssid, passwd):
+        logging.debug("ssid: {}, passwd: {}".format(ssid, passwd))
+        output = run_nmcli_command("nmcli device wifi connect '" + ssid + "' password '" + passwd + "'")
+        logging.debug(output)
+        if output:
+            logging.debug("connectSsid success")
+            return 0
+        else:
+            logging.debug("connectSsid fail")
+            return 1
+    def getActivedWifi():
+        output = run_nmcli_command("nmcli -f IN-USE,SSID device wifi |grep '*       '|sed -n 1p")
+        logging.debug(output)
+        if output:
+            logging.debug("has ActivedWifi")
+            return output[8:]
+        else:
+            logging.debug("no ActivedWifi")
+            return None
+    def connectActivedWifi(ssid, connect):
+        logging.debug("ssid: {}, connect: {}".format(ssid, connect))
+        if connect == 1:
+            output = run_nmcli_command("nmcli dev wifi connect '" + ssid + "'")
+        elif connect == 0:
+            output = run_nmcli_command("nmcli connection down '" + ssid + "'")
+        logging.debug(output)
+        if output:
+            logging.debug("connectActivedWifi success")
+            return 0
+        else:
+            logging.debug("connectActivedWifi fail")
+            return 1
+    def enableWifi(enable):
+        logging.debug("enable: {}".format(enable))
+        if enable == 1:
+            output = run_nmcli_command("nmcli radio wifi on")
+        elif enable == 0:
+            output = run_nmcli_command("nmcli radio wifi off")
+        logging.debug(output)
+        if output:
+            logging.debug("enableWifi success")
+            return 0
+        else:
+            logging.debug("enableWifi fail")
+            return 1
+    def connectedWifiList():
+        output = run_nmcli_command("nmcli -g NAME,TYPE connection show|grep '802-11-wireless'|awk -F: '{print $1}'|sort -u")
+        logging.debug(output)
+        if output == "success":
+            logging.debug("connectedWifiList success none")
+            return None
+        elif output:
+            logging.debug("connectedWifiList success has")
+            return output
+        else:
+            logging.debug("connectedWifiList fail")
+            return None
+    def isWifiEnable():
+        output = run_nmcli_command("nmcli -g TYPE,STATE device status|grep wifi|grep 'wifi:unavailable'")
+        logging.debug(output)
+        if output == "wifi:unavailable":
+            logging.debug("isWifiEnable disable")
+            return 0
+        else:
+            logging.debug("isWifiEnable enable")
+            return 1
+    def getSignalAndSecurity(ssid):
+        output = run_nmcli_command("nmcli -g SSID,SIGNAL,SECURITY dev wifi|grep '" + ssid + "'|awk -F: '{print $2} {print $3}'")
+        logging.debug(output)
+        if output == "success":
+            logging.debug("no SignalAndSecurity")
+            return None
+        elif output:
+            logging.debug("has SignalAndSecurity")
+            return output
+        else:
+            logging.debug("getSignalAndSecurity fail")
+            return None
+    def connectHidedWifi(ssid, passwd):
+        logging.debug("ssid: {}, passwd: {}".format(ssid, passwd))
+
+        output = run_nmcli_command("nmcli dev wifi connect '" + ssid + "' password '" + passwd + "' hidden yes")
+        tryCount = 8
+        logging.debug(output)
+        while not output and tryCount != 0:
+            time.sleep(2)
+            output = run_nmcli_command("nmcli dev wifi connect '" + ssid + "' password '" + passwd + "' hidden yes")
+            tryCount -= 1
+            logging.debug("connectActivedWifi success")
+        if output:
+            logging.debug("connectHidedWifi success")
+            return 0
+        else:
+            logging.debug("connectHidedWifi fail")
+            return 1
+    def service_thread():
+        while not stopping:
+            INet.add_service(
+                args, setStaticIp, setDHCP, getAllSsid, connectSsid, getActivedWifi, connectActivedWifi, 
+                enableWifi, connectedWifiList, isWifiEnable, getSignalAndSecurity, connectHidedWifi)
+
+    global stopping
+    stopping = False
+    args.net_manager = threading.Thread(target=service_thread)
+    args.net_manager.start()
+
+def stop(args):
+    global stopping
+    stopping = True
+    try:
+        if args.netLoop:
+            args.netLoop.quit()
+    except AttributeError:
+        logging.debug("net service is not even started")
