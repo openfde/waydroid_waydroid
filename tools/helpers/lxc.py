bca8cebd077d8fea459e63948d6e0563ea7cce9b
185457686 <185457686@qq.com>
2025-06-13 11:19:44 +0800

    set fde.app_fusion depends by share_shortcut

--- tools/helpers/lxc.py
+++ tools/helpers/lxc.py
@@ -298,6 +298,9 @@ def make_base_props(args):
                                         "/etc/fde.d/fde.conf", "mode")
         if mode == "app_fusing":
             props.append("persist.waydroid.multi_windows=true")
+        share_shortcut = tools.helpers.props.file_get(args,"/etc/fde.d/fde.conf","share_shortcut")
+        if share_shortcut == "false":
+            props.append("fde.app_fusion=0")
 
     media_profiles = tools.helpers.props.host_get(args, "media.settings.xml")
     if media_profiles != "":
