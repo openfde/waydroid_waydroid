f26e2ae95056417754736c1d12e400861fea3242
gongyong <185457686@qq.com>
2025-09-15 11:59:43 +0800

    set hwcomposer depends on xdg_session_type

--- tools/helpers/images.py
+++ tools/helpers/images.py
@@ -133,6 +133,13 @@ def make_prop(args, cfg, full_props_path):
     dpi = cfg["lcd_density"]
     if dpi != "0":
         props.append("ro.sf.lcd_density=" + dpi)
+    if cfg["xdg_session_type"] == "x11":
+        logging.warn("xdg_session type is x11")
+        props.append("ro.hardware.hwcomposer=x11")
+    else:
+        logging.warn("xdg_session type is wayland")
+        props.append("ro.hardware.hwcomposer=openfde")
+
 
     props.append("ro.openfde.version=20230318.111310")
     props.append("ro.product.brand=google")
