bdde27e51d7d1d4af7afd530ac885bcf21a4a61c
185457686 <185457686@qq.com>
2025-09-26 15:18:29 +0800

    set default lcd density to 160

--- tools/helpers/lxc.py
+++ tools/helpers/lxc.py
@@ -293,6 +293,8 @@ def make_base_props(args):
     props.append("ro.hardware.gralloc=" + gralloc)
     if gralloc == "LEOPARD":
         props.append("ro.sf.lcd_density=220")
+    else:
+        props.append("ro.sf.lcd_density=160")
 
     if egl != "":
         props.append("ro.hardware.egl=" + egl)
