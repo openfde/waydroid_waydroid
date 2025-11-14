e4e5b8d94810dfbbab16331be0a8071ef484b3a5
xudq <xudingqiang520@126.com>
2025-11-14 14:23:35 +0800

    add version field for app list

--- tools/actions/app_manager.py
+++ tools/actions/app_manager.py
@@ -105,6 +105,7 @@ def list(args):
             for app in appsList:
                 print("Name: " + app["name"])
                 print("packageName: " + app["packageName"])
+                print("version: " + app["version"])
                 print("categories:")
                 for cat in app["categories"]:
                     print("\t" + cat)
