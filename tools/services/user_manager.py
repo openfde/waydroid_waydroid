511065a2432e263cef9cee68c772db9d0ebceea2
185457686 <185457686@qq.com>
2026-01-13 17:48:53 +0800

    fix no notification

--- tools/services/user_manager.py
+++ tools/services/user_manager.py
@@ -3,12 +3,14 @@
 import logging
 import os
 import threading
+import subprocess
 import requests
 import tools.config
 import tools.helpers.net
 from tools.interfaces import IUserMonitor
 from tools.interfaces import IPlatform
 from tools.helpers.personal_dirs import get_personal_dirs
+import json
 
 stopping = False
 
@@ -35,6 +37,7 @@ def start(args, session, unlocked_cb=None):
             lines.append("Name=" + appInfo["name"])
             lines.append("Exec=waydroid app launch " + packageName)
             lines.append("Icon=" + waydroid_data + "/icons/" + packageName + ".png")
+            lines.append("NoDisplay=true")
             lines.append("Categories=X-WayDroid-App;")
             lines.append("X-Purism-FormFactor=Workstation;Mobile;")
             lines.append("Actions=app_settings;")
@@ -101,36 +104,31 @@ def start(args, session, unlocked_cb=None):
     def packageStateChanged(mode, packageName, uid):
         logging.debug("packageStateChanged mode: {}, packageName: {}".format(mode, packageName))
         
-        platformService = IPlatform.get_service(args)
-        if platformService:
-            multiwin = platformService.getprop(
-                "persist.waydroid.multi_windows", "false")
-            if multiwin == "false":
-                return
-            if mode == 3:
-                package_info = json.dumps({"PackageName": packageName, "OpCode":"start","Status":"Success"})
+        if mode == 3:
+            package_info = json.dumps({"PackageName": packageName, "OpCode":"start","Status":"Success"})
+            cmd = ['fde_ctrl', '-msg', package_info]
+            threading.Thread(target=lambda: subprocess.run(cmd, check=False)).start()
+        elif mode == 4:
+            package_info = json.dumps({"PackageName": packageName, "OpCode":"stop","Status":"Success"})
+            cmd = ['fde_ctrl', '-msg', package_info]
+            threading.Thread(target=lambda: subprocess.run(cmd, check=False)).start()
+        else:
+            appInfo = platformService.getAppInfo(packageName)
+            desktop_file_path = apps_dir + "/waydroid." + packageName + ".desktop"
+            if mode == 0:
+                # Package added
+                makeDesktopFile(appInfo)
+            elif mode == 1:
+                logging.debug("remove before")
+                package_info = json.dumps({"PackageName": packageName, "OpCode":"remove","Status":"Success"})
                 cmd = ['fde_ctrl', '-msg', package_info]
                 threading.Thread(target=lambda: subprocess.run(cmd, check=False)).start()
-            elif mode == 4:
-                package_info = json.dumps({"PackageName": packageName, "OpCode":"stop","Status":"Success"})
-                cmd = ['fde_ctrl', '-msg', package_info]
-                threading.Thread(target=lambda: subprocess.run(cmd, check=False)).start()
+                if os.path.isfile(desktop_file_path):
+                    os.remove(desktop_file_path)
             else:
-                appInfo = platformService.getAppInfo(packageName)
-                desktop_file_path = apps_dir + "/waydroid." + packageName + ".desktop"
-                if mode == 0:
-                    # Package added
-                    makeDesktopFile(appInfo)
-                elif mode == 1:
-                    package_info = json.dumps({"PackageName": packageName, "OpCode":"remove","Status":"Success"})
-                    cmd = ['fde_ctrl', '-msg', package_info]
-                    threading.Thread(target=lambda: subprocess.run(cmd, check=False)).start()
-                    if os.path.isfile(desktop_file_path):
+                if os.path.isfile(desktop_file_path):
+                    if makeDesktopFile(appInfo) == -1:
                         os.remove(desktop_file_path)
-                else:
-                    if os.path.isfile(desktop_file_path):
-                        if makeDesktopFile(appInfo) == -1:
-                            os.remove(desktop_file_path)
 
     def packageStateChangedHasVernsion(mode, packageName,version, uid):
         if('###' in version) :
