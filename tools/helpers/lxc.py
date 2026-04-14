90595969663c817a32b1305bb56a565066036e7c
gongyong <185457686@qq.com>
2026-04-14 15:28:15 +0800

    support ion0

--- tools/helpers/lxc.py
+++ tools/helpers/lxc.py
@@ -49,6 +49,8 @@ def generate_nodes_lxc_config(args):
         make_entry("/dev/dma_heap/system")
     if os.path.exists("/dev/fdeion"):
         make_entry("/dev/fdeion","dev/ion")
+    elif: os.path.exists("/dev/ion0")
+        make_entry("/dev/ion0","/dev/ion")
     else:
         make_entry("/dev/ion")
     make_entry("/dev/tty")
