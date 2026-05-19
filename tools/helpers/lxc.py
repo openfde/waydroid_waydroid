b0fc2f8993b88e471b0d5e37b247bd9e21815cd1
warlice <185457686@qq.com>
2026-05-19 18:17:34 +0800

    Correct entry path for /dev/ion0
    Fix the path in make_entry for /dev/ion0.

--- tools/helpers/lxc.py
+++ tools/helpers/lxc.py
@@ -50,7 +50,7 @@ def generate_nodes_lxc_config(args):
     if os.path.exists("/dev/fdeion"):
         make_entry("/dev/fdeion","dev/ion")
     elif os.path.exists("/dev/ion0"):
-        make_entry("/dev/ion0","/dev/ion")
+        make_entry("/dev/ion0","dev/ion")
     else:
         make_entry("/dev/ion")
     make_entry("/dev/tty")
